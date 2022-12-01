c2d90218e0bd55b57eac10f8caeb082c3d02fc91
Mintchodan <111052779+Mintchodan@users.noreply.github.com>
2022-12-02 03:20:58 +0900

    Spring 세팅 & hello world

---
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data" , "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("home")
+    public String home(Model model) {
+        model.addAttribute("home" , "my sweet spring home");
+        return "home";
+    }
+}
